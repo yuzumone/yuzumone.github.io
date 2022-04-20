ef99876d2a16c40655fa1d3d12c4b502850472cf
yuzumone <yuzumone@users.noreply.github.com>
2022-04-20 19:17:10 +0900

    Use auto route (#2)

---
+++ lib/ui/app/app_page.dart
@@ -0,0 +1,41 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:portfolio/ui/route/app_router.dart';
+
+class AppPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('yuzumone'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              context.navigateTo(const MainRoute());
+            },
+            child: Text('Main'),
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              context.navigateTo(const ResumeRoute());
+            },
+            child: Text('Resume'),
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: AutoRouter(),
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 8.0),
+        child: Text('© 2022 yuzumone'),
+      ),
+    );
+  }
+}
