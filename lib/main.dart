97f4ee1188a250b5d90ebcd919ccd17aa57c7c7f
yuzumone <yuzumone@outlook.com>
2024-12-10 18:59:21 +0900

    Add license

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/ui/route/app_router.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   GoogleFonts.config.allowRuntimeFetching = kDebugMode;
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(MyApp());
 }
 
--- lib/ui/app/app_page.dart
+++ lib/ui/app/app_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio/ui/route/app_router.gr.dart';
 
 @RoutePage()
@@ -35,8 +36,24 @@ class AppPage extends StatelessWidget {
         ),
         body: AutoRouter(),
         bottomNavigationBar: Container(
-          margin: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 8.0),
-          child: Text('© 2022 yuzumone'),
+          margin: const EdgeInsets.only(
+              left: 16.0, right: 16.0, bottom: 8.0, top: 8.0),
+          child: Row(
+            children: [
+              Text('© 2024 yuzumone'),
+              Expanded(child: SizedBox()),
+              Tooltip(
+                message: 'License',
+                child: IconButton(
+                  icon: FaIcon(FontAwesomeIcons.circleInfo),
+                  iconSize: 16,
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  onPressed: () => showLicensePage(context: context),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
