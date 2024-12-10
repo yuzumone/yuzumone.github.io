f3f32b7e4f9732a669d991c5fa55849f91ff6698
yuzumone <yuzumone@users.noreply.github.com>
2024-12-10 19:08:18 +0900

    Fix google fonts (#9)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/ui/route/app_router.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  GoogleFonts.config.allowRuntimeFetching = kDebugMode;
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(MyApp());
 }
 
@@ -16,7 +24,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         brightness: Brightness.dark,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: GoogleFonts.notoSansTextTheme(
+        textTheme: GoogleFonts.notoSansJpTextTheme(
           ThemeData.dark().textTheme,
         ),
       ),
--- tool/grind.dart
+++ tool/grind.dart
@@ -4,11 +4,76 @@ import 'package:grinder/grinder.dart';
 
 main(args) => grind(args);
 
+@Task('Get google fonts for release build')
+void fonts() async {
+  var notoSansJp = [
+    {
+      'name': 'NotoSansJP-Thin.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFBEi75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-ExtraLight.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFJEj75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-Light.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFE8j75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-Regular.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFBEj75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-Medium.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFCMj75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-SemiBold.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFM8k75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-Bold.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFPYk75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-ExtraBold.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFJEk75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-Black.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F6jfjtqLzI2JPCgQBnw7HFyzSD-AsregP8VFLgk75vY0rw-oME.ttf'
+    },
+    {
+      'name': 'NotoSansJP-VariableFont_wght.ttf',
+      'url':
+          'https://fonts.gstatic.com/s/notosansjp/v53/-F62fjtqLzI2JPCgQBnw7HFoxgIO2lZ9hg.ttf'
+    },
+  ];
+  for (var f in notoSansJp) {
+    await Process.start('curl', [
+      '-o',
+      'google_fonts/${f["name"]}',
+      f['url']!,
+    ]);
+    print('Download ${f["name"]}');
+  }
+}
+
 @Task('flutter pub get')
-pubGet() async => _flutter(['packages', 'pub', 'get']);
+void pubGet() async => _flutter(['packages', 'pub', 'get']);
 
 @Task('flutter build web')
-build() async {
+@Depends(fonts)
+void build() async {
   await _flutter(['build', 'web', '--web-renderer', 'html']);
   final robots = '''User-agent: *
 Disallow:
@@ -16,8 +81,8 @@ Disallow:
   File('./build/web/robots.txt').writeAsStringSync(robots);
 }
 
-@Task('flutter format')
-void format() async => await _flutter(['format']);
+@Task('dart format')
+void format() async => await _dart(['format', '.']);
 
 @Task('flutter analyze')
 void analyze() async => await _flutter(['analyze']);
@@ -27,11 +92,24 @@ void test() async => await _flutter(['test']);
 
 @Task('flutter analyze, test and format check')
 @Depends(pubGet, analyze, test)
-void lint() async => await _flutter(['format', '.', '--set-exit-if-changed']);
+void lint() async => await _dart(['format', '.', '--set-exit-if-changed']);
 
 @DefaultTask()
 @Depends(lint, build)
 void task() {}
+
+Future<void> _dart(List<String> arguments) async {
+  final fvm = await Process.run('which', ['fvm']);
+  if (fvm.exitCode == 0) {
+    final result = await Process.start('fvm', ['dart', ...arguments]);
+    await stdout.addStream(result.stdout);
+    await stderr.addStream(result.stderr);
+  } else {
+    final result = await Process.start('dart', arguments);
+    await stdout.addStream(result.stdout);
+    await stderr.addStream(result.stderr);
+  }
+}
 
 Future<void> _flutter(List<String> arguments) async {
   final fvm = await Process.run('which', ['fvm']);
